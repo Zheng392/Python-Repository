a942d0b6fbfa6d59d7db50c1c611cdb7ca2d1f2f
Zheng392 <724139392@qq.com>
2018-12-26 18:49:31 +0800

    Update rename.py

--- download pdf/rename.py
+++ download pdf/rename.py
@@ -6,17 +6,25 @@ for i in range(4):
     for j in range(6):
         path = 'vol%d/issue%d'%(51+i,1+j)
         for file in os.listdir(path):
-            pdfFileReader = PdfFileReader(os.path.join(path,file))
+            if '.pdf' in file:
+                pdfFileReader = PdfFileReader(os.path.join(path,file))
+            else:
+                continue
+            try:
+                documentInfo = pdfFileReader.getDocumentInfo()
+            except Exception:
+                continue
             documentInfo = pdfFileReader.getDocumentInfo()
-            name = documentInfo['/Title']
-            name=name.replace('/',' ')
-            name = name.replace(':', ' ')
-            name = name.replace('\\', ' ')
-            name = name.replace('*', ' ')
-            name = name.replace('?', ' ')
-            name = name.replace('"', ' ')
-            name = name.replace('<', ' ')
-            name = name.replace('>', ' ')
-            name = name.replace('|', ' ')
-            print(os.path.join(path, file))
-            os.rename(os.path.join(path, file), os.path.join(path,name+".pdf"))
+            if documentInfo.get('/Title'):
+                name = documentInfo['/Title']
+                name=name.replace('/',' ')
+                name = name.replace(':', ' ')
+                name = name.replace('\\', ' ')
+                name = name.replace('*', ' ')
+                name = name.replace('?', ' ')
+                name = name.replace('"', ' ')
+                name = name.replace('<', ' ')
+                name = name.replace('>', ' ')
+                name = name.replace('|', ' ')
+                print(os.path.join(path, file))
+                os.rename(os.path.join(path, file), os.path.join(path,name+".pdf"))
